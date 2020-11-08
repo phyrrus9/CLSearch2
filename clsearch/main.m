bfa9eaf71089cf0860f2db656f8d552324b3735b
Ethan Laur <phyrrus9@gmail.com>
2020-11-08 13:53:30 -0700

    Initial Commit

---
+++ clsearch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  clsearch
+//
+//  Created by Ethan on 11/8/20.
+//  Copyright © 2020 Ipseity Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
